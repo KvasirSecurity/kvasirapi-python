ec8c5818bd5913f3afd150f25eaec6e7cc732f4c
Kurt Grutzmacher <grutz@jingojango.net>
2014-08-16 21:03:50 -0700

    version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='KvasirAPI',
-    version='1.0.0rc1',
+    version='1.0.0',
     packages=['KvasirAPI', 'KvasirAPI/jsonrpc'],
     url='https://github.com/KvasirSecurity/kvasirapi-python',
     license='BSD',
